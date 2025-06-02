1cd4d7ceaf9ec09ee70b1768e60955bf4a594307
JoãoFeliciano <felicianojoao777@gmail.com>
2025-06-02 14:48:34 -0300

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Test1 {
+    public static void main(String[] args ) {
+        Date data = new Date();
+        System.out.println("The system time is: ");
+        System.out.println(data.toString());        
+    }
+
+    
+}
---
+++ Test2.java
@@ -0,0 +1,12 @@
+
+import java.util.Locale;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Locale language = Locale.getDefault();
+        System.out.println("The language of system is:");
+        System.out.println(language.toString());
+        
+        
+    }
+}
